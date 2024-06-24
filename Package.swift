632803fea3f6389ad2872b005a963227d62118cd
Loyle <slsnsi1212@gmail.com>
2024-06-24 18:57:41 +0900

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "AFOAuth2Manager",
+    platforms: [
+        .iOS(.v7),
+        .macOS(.v10_9),
+        .tvOS(.v9),
+        .watchOS(.v2)
+    ],
+    products: [
+        .library(
+            name: "AFOAuth2Manager",
+            targets: ["AFOAuth2Manager"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/AFNetworking/AFNetworking.git", .upToNextMajor(from: "4.0.0")),
+    ],
+    targets: [
+        .target(
+            name: "AFOAuth2Manager",
+            dependencies: [
+                .product(name: "AFNetworking", package: "AFNetworking")
+            ],
+            path: "AFOAuth2Manager",
+            exclude: [],
+            resources: [],
+            publicHeadersPath: ".",
+            cSettings: [
+                .headerSearchPath(".")
+            ]
+        )
+    ]
+)
